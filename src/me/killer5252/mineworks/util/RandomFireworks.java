746dce284b8bb8761084f3c87a0e55851ff4d30f
Killer5252 <killer5252mc@gmail.com>
2014-05-12 16:39:52 -0700

    Cleanup

--- src/me/killer5252/mineworks/util/RandomFireworks.java
+++ src/me/killer5252/mineworks/util/RandomFireworks.java
@@ -8,36 +8,38 @@ import org.bukkit.Location;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
 
-
-
 public class RandomFireworks {
 	
 	private static RandomFireworks fireWorks = new RandomFireworks();
 	
-	Random rnd = new Random();
-	
 	public static RandomFireworks getManager(){
 		return fireWorks;
 	}
+
+	Random rnd = new Random();
 	private Color[] colors = { Color.AQUA, Color.BLACK, Color.BLUE, Color.FUCHSIA, 
 			Color.GREEN, Color.LIME, Color.MAROON, Color.OLIVE, Color.ORANGE, 
 			Color.PURPLE, Color.RED, Color.TEAL, Color.WHITE, Color.YELLOW };
 	
 	public void launchRandomFirework(Location loc){
 		Firework fw = loc.getWorld().spawn(loc, Firework.class);
-        FireworkMeta fm = fw.getFireworkMeta();
+		FireworkMeta fm = fw.getFireworkMeta();
         FireworkEffect.Builder effect = FireworkEffect.builder();
-        effect.flicker(this.rnd.nextInt(10) > 2);
-        effect.trail(this.rnd.nextInt(10) > 2);
         FireworkEffect.Type[] types = FireworkEffect.Type.values();
-        effect.with(types[this.rnd.nextInt(types.length)]);
-        int colorcnt = this.rnd.nextInt(3) + 2;
-        for (int i = 0; i < colorcnt; i++) {
-          effect.withColor(this.colors[this.rnd.nextInt(this.colors.length)]);
+        
+        int colorcnt = rnd.nextInt(3) + 2;
+        int rp = rnd.nextInt(4) + 1;
+        
+        effect.flicker(rnd.nextInt(10) > 2);
+        effect.trail(rnd.nextInt(10) > 2);
+        effect.with(types[rnd.nextInt(types.length)]);
+       
+        for (int i = 0; i < colorcnt; i++){
+        	effect.withColor(colors[rnd.nextInt(colors.length)]);
         }
+        
         fm.addEffect(effect.build());
-        int rp = rnd.nextInt(2) + 1;
-		fm.setPower(rp);
+        fm.setPower(rp);
         fw.setFireworkMeta(fm);
 	}
 }
