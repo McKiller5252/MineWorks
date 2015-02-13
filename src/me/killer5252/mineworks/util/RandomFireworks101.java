28c9282acd0f7ed29fae62300ebc011be4127e0b
Killer5252™ <killer5252mc@gmail.com>
2015-02-12 20:32:33 -0800

    Update RandomFireworks101.java

--- src/me/killer5252/mineworks/util/RandomFireworks101.java
+++ src/me/killer5252/mineworks/util/RandomFireworks101.java
@@ -24,22 +24,22 @@ public class RandomFireworks101 {
 	public void launchRandomFirework(Location loc){
 		Firework fw = loc.getWorld().spawn(loc, Firework.class);
 		FireworkMeta fm = fw.getFireworkMeta();
-        FireworkEffect.Builder effect = FireworkEffect.builder();
-        FireworkEffect.Type[] types = FireworkEffect.Type.values();
+                FireworkEffect.Builder effect = FireworkEffect.builder();
+                FireworkEffect.Type[] types = FireworkEffect.Type.values();
         
-        int colorcnt = rnd.nextInt(3) + 2;
-        int rp = rnd.nextInt(4) + 1;
+         int colorcnt = rnd.nextInt(3) + 2;
+         int rp = rnd.nextInt(4) + 1;
         
-        effect.flicker(rnd.nextInt(10) > 2);
-        effect.trail(rnd.nextInt(10) > 2);
-        effect.with(types[rnd.nextInt(types.length)]);
+           effect.flicker(rnd.nextInt(10) > 2);
+           effect.trail(rnd.nextInt(10) > 2);
+           effect.with(types[rnd.nextInt(types.length)]);
        
         for (int i = 0; i < colorcnt; i++){
         	effect.withColor(colors[rnd.nextInt(colors.length)]);
         }
         
-        fm.addEffect(effect.build());
-        fm.setPower(rp);
-        fw.setFireworkMeta(fm);
-	}
+          fm.addEffect(effect.build());
+          fm.setPower(rp);
+          fw.setFireworkMeta(fm);
+    }
 }
